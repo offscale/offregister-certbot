51e1efb9b78f3207592be217c8625c6342fefb16
Samuel Marks <samuelmarks@gmail.com>
2018-10-07 17:25:45 +1100

    Implemented `apply_cert2`: which updates the nginx conf (parsing & emission)

--- offregister_certbot/ubuntu/__init__.py
+++ offregister_certbot/ubuntu/__init__.py
@@ -1,13 +1,19 @@
+from cStringIO import StringIO
 from functools import partial
 from itertools import imap
+from os import remove
 from sys import modules
+from tempfile import mkstemp
 
+import offregister_nginx_static.ubuntu as nginx
 from fabric.context_managers import cd
-from fabric.operations import _run_command, sudo
-
+from fabric.contrib.files import exists
+from fabric.operations import _run_command, sudo, get, put
+from nginx_parse_emit import emit as nginx_emit
+from nginx_parse_emit.utils import apply_attributes
+from nginxparser import dumps, load, loads
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.ubuntu.systemd import restart_systemd
-import offregister_nginx_static.ubuntu as nginx
 
 from offregister_certbot import get_logger
 
@@ -33,6 +39,7 @@ def add_cert1(domains, email, server='nginx', **kwargs):
 
     sites_enabled = kwargs.get('sites-enabled', '/etc/nginx/sites-enabled')
     sites_disabled = kwargs.get('sites-disabled', '/etc/nginx/sites-disabled')
+    sudo('mkdir -p {sites_disabled}'.format(sites_disabled=sites_disabled))
 
     cmd = partial(_run_command, user=kwargs.get('as_user', 'root'), group=kwargs.get('as_group', 'root'),
                   shell_escape=False, sudo=True)
@@ -69,3 +76,34 @@ def add_cert1(domains, email, server='nginx', **kwargs):
                                                        sites_enabled=sites_enabled)
                  for conf in confs.split('\n')))
     return restart_systemd('nginx')  # reload didn't work :(
+
+
+def apply_cert2(domains, use_sudo=True, **kwargs):
+    certs_dirname = frozenset(d for d in sudo('ls -AQlx /etc/letsencrypt/live').split('"') if len(d.strip()))
+
+    def apply_cert(domain):
+        assert domain in certs_dirname, '{domain} doesn\'t have certs generated'.format(domain=domain)
+        conf_name = '/etc/nginx/sites-enabled/{nginx_conf}'.format(nginx_conf=domain)
+        if not conf_name.endswith('.conf') and not exists(conf_name):
+            conf_name += '.conf'
+        # cStringIO.StringIO, StringIO.StringIO, TemporaryFile, SpooledTemporaryFile all failed :(
+        tempfile = mkstemp(domain)[1]
+        get(remote_path=conf_name, local_path=tempfile, use_sudo=use_sudo)
+        with open(tempfile, 'rt') as f:
+            conf = load(f)
+        new_conf = apply_attributes(conf,
+                                    nginx_emit.secure_attr('/etc/letsencrypt/live/{domain}/fullchain.pem'.format(domain=domain),
+                                                '/etc/letsencrypt/live/{domain}/privkey.pem'.format(domain=domain)))
+        remove(tempfile)
+
+        new_conf = loads(nginx_emit.redirect_block(server_name=domain, port=80)) + new_conf
+
+        sio = StringIO()
+        sio.write(dumps(new_conf))
+        put(sio, conf_name, use_sudo=use_sudo)
+        return conf_name
+
+    r = tuple(imap(apply_cert, domains))
+
+    restart_systemd('nginx')
+    return {'updated_ssl_info': r}
